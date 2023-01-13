9ebfa980d4e58885ce2af4a0d4ebacca469006c7
Sandipan Pal <sandipanpal010@gmail.com>
2023-01-13 16:47:18 +0530

    Linq command tested

---
+++ LinqTest/LinqTest/EvenArrayElements.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqTest
+{
+    public class EvenArrayElements
+    {
+        public void Display()
+        {
+            List<int> list = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 12 };
+            var result = (from indx in list
+                          where indx % 2 == 0
+                          select indx).ToList();
+            foreach (var item in result)
+                Console.WriteLine(item);
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ LinqTest/LinqTest/Program.cs
@@ -0,0 +1,5 @@
+﻿using LinqTest;
+
+EvenArrayElements obj1 = new EvenArrayElements();
+obj1.Display();
+Console.ReadKey();
